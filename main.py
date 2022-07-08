8b100080b343e3f68da415e5a5129283a5e9fe70
Sushi <github@sushipython.us>
2022-07-08 00:12:20 -0400

    finish backend

--- main.py
+++ main.py
@@ -12,27 +12,8 @@ import re
 app = Flask(__name__) # init app
 dotenv.load_dotenv() # make sure you have an env file
 
-def gen_rand_str(len):
-    return ''.join(random.choice(string.ascii_letters) for _ in range(len))
-
-auth_manager=SpotifyOAuth(
-    client_id=os.getenv('SPOTIPY_CLIENT_ID'), #env file
-    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'), #env file
-    redirect_uri="http://localhost:8080/auth", #change this to whatever port
-    scope="user-top-read", #reading top songs
-    cache_path=f"cache/.cache-spotipy-{gen_rand_str(10)}" # spotipy forces me to cache this
-)
-url = auth_manager.get_authorize_url()
-
-@app.route('/')
-def index():
-    return render_template('index.html', url=url)
-
-@app.route('/get_lyrics')
-def get_lyrics():
-    song = request.args['song']
-
-    genius_api = requests.get('https://genius.com/api/search/multi?per_page=1&q=' + song).json()
+def get_lyrics(query):
+    genius_api = requests.get(f'https://genius.com/api/search/multi?per_page=1&q={query}').json()
     #print(genius_api)
     song_name = genius_api['response']['sections'][0]['hits'][0]['result']['title']
     song_artist = genius_api['response']['sections'][0]['hits'][0]['result']['artist_names']
@@ -59,6 +40,29 @@ def get_lyrics():
         'lyrics': lines
     }
 
+def gen_rand_str(len):
+    return ''.join(random.choice(string.ascii_letters) for _ in range(len))
+
+auth_manager=SpotifyOAuth(
+    client_id=os.getenv('SPOTIPY_CLIENT_ID'), #env file
+    client_secret=os.getenv('SPOTIPY_CLIENT_SECRET'), #env file
+    redirect_uri="http://localhost:8080/auth", #change this to whatever port
+    scope="user-top-read", #reading top songs
+    cache_path=f"cache/.cache-spotipy-{gen_rand_str(10)}" # spotipy forces me to cache this
+)
+url = auth_manager.get_authorize_url()
+
+@app.route('/')
+def index():
+    return render_template('index.html', url=url)
+
+@app.route('/api/get_lyrics')
+def api_get_lyrics():
+    song = request.args['song']
+    artist = request.args['artist']
+    lyrics = get_lyrics(f'{song} {artist}')
+    return lyrics
+
 
 
 @app.route('/auth')
@@ -70,17 +74,22 @@ def auth():
 
     songs = []
 
-    results = sp.current_user_top_tracks(time_range='short_term', limit=50)
+    results = sp.current_user_top_tracks(time_range='short_term', limit=500)
+    all_songs = []
     for idx, item in enumerate(results['items']): # each item is a song
         song_data = {
             'id': item['id'],
             'name': item['name'],
             'artist': item['artists'][0]['name'],
             'album': item['album']['name'],
-            "album_art": item['album']['images'][0]['url']
+            "album_art": item['album']['images'][0]['url'],
+            "name_lower": item['name'].lower(),
         }
         songs.append(song_data)
+        all_songs.append(item['name'])
     random_song = random.choice(songs)
-    return render_template('app.html', song=random_song)
+    random_song['lyrics'] = get_lyrics(f"{random_song['name']} {random_song['artist']}")
+    print(all_songs)
+    return render_template('app.html', song=random_song, all_songs=str(all_songs))
 
 app.run(host='0.0.0.0', port=8080) # start app
